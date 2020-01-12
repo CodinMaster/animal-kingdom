31280bc162d68794cf7ec0e8c23091c12dfbe1f9
CodinMaster <latheyapoorv@gmail.com>
2020-01-12 12:40:22 +0530

    cors config in build

---
+++ build/precache-manifest.fbebc4427ea6a1ca865ab1dd89afc275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8cf62a3dd59d4165b640060c58dd375",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a891e773381e4c345c6",
+    "url": "/static/css/2.9dafe002.chunk.css"
+  },
+  {
+    "revision": "2c79b71da799a1c79e5e",
+    "url": "/static/css/main.235aa368.chunk.css"
+  },
+  {
+    "revision": "9a891e773381e4c345c6",
+    "url": "/static/js/2.b7dcff0b.chunk.js"
+  },
+  {
+    "revision": "2c79b71da799a1c79e5e",
+    "url": "/static/js/main.20e90250.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
